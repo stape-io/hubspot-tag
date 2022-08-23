42f6ae523f55ed96e4538fd1e07e5e3c545104f8
Denis Golubovskiy <bukashk0zzz@me.com>
2022-08-23 15:41:35 +0200

    Update HubSpot Auth and Add logs

--- template.js
+++ template.js
@@ -2,11 +2,12 @@
 const encodeUriComponent = require('encodeUriComponent');
 const getAllEventData = require('getAllEventData');
 const JSON = require('JSON');
+const getRequestHeader = require('getRequestHeader');
 
 const logToConsole = require('logToConsole');
 const getContainerVersion = require('getContainerVersion');
-const containerVersion = getContainerVersion();
-const isDebug = containerVersion.debugMode;
+const isLoggingEnabled = determinateIsLoggingEnabled();
+const traceId = getRequestHeader('trace-id');
 
 let type = data.type;
 const eventData = getAllEventData();
@@ -32,7 +33,30 @@ function trackEventPageView() {
   if (eventData.page_location) url + '&pu=' + encodeUriComponent(eventData.page_location);
   if (eventData.screen_resolution) url + '&sd=' + encodeUriComponent(eventData.screen_resolution);
 
+  if (isLoggingEnabled) {
+    logToConsole(JSON.stringify({
+      'Name': 'HubSpot',
+      'Type': 'Request',
+      'TraceId': traceId,
+      'EventName': 'page_view',
+      'RequestMethod': 'GET',
+      'RequestUrl': url,
+    }));
+  }
+
   sendHttpRequest(url, (statusCode, headers, body) => {
+    if (isLoggingEnabled) {
+      logToConsole(JSON.stringify({
+        'Name': 'HubSpot',
+        'Type': 'Response',
+        'TraceId': traceId,
+        'EventName': 'page_view',
+        'ResponseStatusCode': statusCode,
+        'ResponseHeaders': headers,
+        'ResponseBody': body,
+      }));
+    }
+
     if (statusCode >= 200 && statusCode < 300) {
       data.gtmOnSuccess();
     } else {
@@ -43,7 +67,7 @@ function trackEventPageView() {
 
 
 function trackCustomBehavioralEvent() {
-  let url = 'https://api.hubapi.com/events/v3/send?hapikey='+ encodeUriComponent(data.apiKey);
+  let url = 'https://api.hubapi.com/events/v3/send';
   let bodyData = {
     'eventName': data.customBehavioralEventEventName,
     'properties': mapProperties(data.customBehavioralEventParameters),
@@ -54,36 +78,75 @@ function trackCustomBehavioralEvent() {
   if (data.customBehavioralEventObjectId) bodyData.objectId = data.customBehavioralEventObjectId;
   if (data.customBehavioralEventOccurredAt) bodyData.occurredAt = data.customBehavioralEventOccurredAt;
 
-  if (isDebug) {
-    logToConsole('HubSpot trackCustomBehavioralEvent data: ', bodyData);
+  if (isLoggingEnabled) {
+    logToConsole(JSON.stringify({
+      'Name': 'HubSpot',
+      'Type': 'Request',
+      'TraceId': traceId,
+      'EventName': data.customBehavioralEventEventName,
+      'RequestMethod': 'POST',
+      'RequestUrl': url,
+      'RequestBody': bodyData,
+    }));
   }
 
   sendHttpRequest(url, (statusCode, headers, body) => {
+    if (isLoggingEnabled) {
+      logToConsole(JSON.stringify({
+        'Name': 'HubSpot',
+        'Type': 'Response',
+        'TraceId': traceId,
+        'EventName': data.customBehavioralEventEventName,
+        'ResponseStatusCode': statusCode,
+        'ResponseHeaders': headers,
+        'ResponseBody': body,
+      }));
+    }
+
     if (statusCode >= 200 && statusCode < 300) {
       data.gtmOnSuccess();
     } else {
       data.gtmOnFailure();
     }
-  }, {headers: {'content-type': 'application/json'}, method: 'POST', timeout: 3500}, JSON.stringify(bodyData));
+  }, {headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer '+data.apiKey}, method: 'POST'}, JSON.stringify(bodyData));
 }
 
 function createOrUpdateContact() {
-  let url = 'https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/'+encodeUriComponent(data.email)+'/?hapikey='+ encodeUriComponent(data.apiKey);
+  let url = 'https://api.hubapi.com/contacts/v1/contact/createOrUpdate/email/'+encodeUriComponent(data.email)+'/';
   let bodyData = {
     'properties': data.contactParameters
   };
 
-  if (isDebug) {
-    logToConsole('HubSpot createOrUpdateContact data: ', bodyData);
+  if (isLoggingEnabled) {
+    logToConsole(JSON.stringify({
+      'Name': 'HubSpot',
+      'Type': 'Request',
+      'TraceId': traceId,
+      'EventName': 'contact_create_or_update',
+      'RequestMethod': 'POST',
+      'RequestUrl': url,
+      'RequestBody': bodyData,
+    }));
   }
-
   sendHttpRequest(url, (statusCode, headers, body) => {
+    if (isLoggingEnabled) {
+      logToConsole(JSON.stringify({
+        'Name': 'HubSpot',
+        'Type': 'Response',
+        'TraceId': traceId,
+        'EventName': 'contact_create_or_update',
+        'ResponseStatusCode': statusCode,
+        'ResponseHeaders': headers,
+        'ResponseBody': body,
+      }));
+    }
+
     if (statusCode >= 200 && statusCode < 300) {
       data.gtmOnSuccess();
     } else {
       data.gtmOnFailure();
     }
-  }, {headers: {'content-type': 'application/json'}, method: 'POST', timeout: 3500}, JSON.stringify(bodyData));
+  }, {headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer '+data.apiKey}, method: 'POST'}, JSON.stringify(bodyData));
 }
 
 function mapProperties(parameters) {
@@ -94,4 +157,26 @@ function mapProperties(parameters) {
   }
 
   return result;
+}
+
+function determinateIsLoggingEnabled() {
+  const containerVersion = getContainerVersion();
+  const isDebug = !!(
+      containerVersion &&
+      (containerVersion.debugMode || containerVersion.previewMode)
+  );
+
+  if (!data.logType) {
+    return isDebug;
+  }
+
+  if (data.logType === 'no') {
+    return false;
+  }
+
+  if (data.logType === 'debug') {
+    return isDebug;
+  }
+
+  return data.logType === 'always';
 }
